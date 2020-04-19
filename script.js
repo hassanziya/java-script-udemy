0fe298347c8e25c8ce0bf8c01bf7e96126c3850c
Hassan Ziya <hassan.ziyakhan86@gmail.com>
2020-04-19 16:23:18 +0530

    AfterNoon programming

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log("hello World again");
+
+// **********Variables**********
+
+var firstName = "john"; //String dataType
+console.log(firstName);
+
+var lastName = "Smith";
+var age = 26; //number Datatype
+
+var fullAge = true; //Boolean dataType
+console.log(fullAge);
+
+var job; //Undefined dataType
+console.log(job);
