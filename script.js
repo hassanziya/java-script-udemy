28ea122c734bb5451a258f6da876505c85cfb9a5
Hassan Ziya <hassan.ziyakhan86@gmail.com>
2020-04-22 02:22:13 +0530

    night coding day2

--- script.js
+++ script.js
@@ -276,3 +276,27 @@ console.log(firstName + " " + lastName);*/
 // };
 // john.calcAge();
 // console.log(john);
+
+/**
+ *************Loops And iteration****************
+ */
+
+// for (var i = 1; i <= 20; i += 2) {
+//   console.log(i);
+// }
+
+// var john = ["John", "Smith", 1990, "designer", false];
+
+// for (var i = 0; i < john.length; i++) {
+//   console.log(john[i]);
+// }
+
+// for (i = john.length - 1; i >= 0; i--) {
+//   console.log(john[i]);
+// }
+
+// var i = 0;
+// while (i < john.length) {
+//   console.log(john[i]);
+//   i++;
+// }
